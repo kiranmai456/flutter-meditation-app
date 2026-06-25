c0aae855f0bd699aee76553b159f75ec9c16104a
SwathiN-tech <swathinatte1234@gmail.com>
2026-06-25 23:27:37 +0530

    Initial commit

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFFF9F8FD);
+  static const text = Color(0xFF303030);
+  static const primary = Color(0xFF817DC0);
+  static const blueLight = Color(0xFFE3E7FF);
+  static const shadow = Color(0xFFE6E6E6);
+}
